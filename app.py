2e5355c832ff5a6b08d7f5a5abdb69eca6ebebb7
rich17lucas <richard.lucas@caymancomputers.co.uk>
2020-05-03 18:23:41 +0100

    Logging changes

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, json
 import logging
 import socket
+
+
 if "RPI" in socket.gethostname().upper():
     from libs.servocontrol import servocontroller as sc
 
-
+def configure_logging():
+    """ Configures the logging for this application"""
+    logging.basicConfig(level=logging.INFO, 
+                        format='%(asctime)s %(name)s %(levelname)s %(message)s',
+                        filename='app.log',
+                        filemode='w')
+    
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,7 +26,7 @@ def hello():
 
 
 @app.route('/gimbal')
-def try_boot_strap():
+def gimbalcontrol():
     return render_template('gimbalcontrol.html')
 
 
@@ -29,27 +37,22 @@ def post_values():
     up_down = request.args.get('up_down') #if request.args.get('up_down') else None
     click = request.args.get('click')
 
-    logging.info(f"left_right: {left_right}")
-    logging.info(f"up_down: {up_down}")
-    logging.info(f"click: {click}")
+    logging.debug(f"left_right: {left_right}")
+    logging.debug(f"up_down: {up_down}")
+    logging.debug(f"click: {click}")
 
     servo_positions = {'left_right':left_right,
                        'up_down':up_down, 
                        'click':click
                        }
     
-    logging.info(servo_positions)
-
-    #try:
+    logging.debug(servo_positions)
     sc.set_servo(servo_positions)
-    #except Exception as error:
-    #    logging.ERROR(error.args())
-
 
     return json.dumps({'status':'OK'})
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', filename='app.log')
     logger = logging.getLogger(__name__)
+    configure_logging()
     app.run(debug=True,port=1080,host="0.0.0.0")
--- libs/servocontrol/servocontroller.py
+++ libs/servocontrol/servocontroller.py
@@ -1,24 +1,24 @@
 from libs.piconzero import piconzero as pz 
 import time
-#import logging
-#logging.basicConfig(level=logging.INFO)
-#mlogger = logging.getLogger(__name__)
+import logging
 
-print("Setting up servo pins")
+sc_logger = logging.getLogger(__name__)
+
+sc_logger.info("Setting up servo pins")
 # Define which pins are the servos
 pan = 1
 tilt = 0
 click = 2
 
-print("Initialising PiconBoard")
+sc_logger.info("Initialising PiconBoard")
 pz.init()
 
-print("Setting output mode to Servo")
+sc_logger.info("Setting output mode to Servo")
 pz.setOutputConfig(pan, 2)
 pz.setOutputConfig(tilt, 2)
 pz.setOutputConfig(click, 2)
 
-print("Centre all servos")
+sc_logger.info("Centre all servos")
 panVal = 90
 tiltVal = 90
 clickVal = 90
@@ -31,25 +31,25 @@ def set_servo(pos):
     
     try:
         panVal = int(pos.get('left_right'))
-        print(f'panVal {panVal}')
+        sc_logger.debug(f'panVal {panVal}')
         pz.setOutput(pan, panVal)
     except TypeError:
         pass
     except Exception as error:
-        print(error)
+        sc_logger.error(error)
     
     try:
         tiltVal = int(pos.get('up_down'))
-        print(f"tiltval: {tiltVal}")
+        sc_logger.debug(f"tiltval: {tiltVal}")
         pz.setOutput(tilt, tiltVal)
     except TypeError:
         pass
     except Exception as error:
-        print(error)
+        sc_logger.error(error)
 
     try:
         clickVal = pos.get('click')
-        print(f"clickVal: {clickVal}")
+        sc_logger.debug(f"clickVal: {clickVal}")
         if clickVal == 'YES':
             pz.setOutput(click, 55)
             time.sleep(0.5)
@@ -57,4 +57,4 @@ def set_servo(pos):
     except TypeError:
         pass
     except Exception as error:
-        print(error)
+        sc_logger.error(error)
